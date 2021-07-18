476bdf3858d43471c7c7d1ff24efcebb1475ed3c
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-07-18 14:57:42 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,29 +1,47 @@
 const inputNumber = document.querySelector("#controls input");
+console.log(inputNumber);
 const renderBtn = document.querySelector('[data-action = render]');
+console.log(renderBtn);
 const destroyBtn = document.querySelector('[data-action = destroy]');
+console.log(destroyBtn);
 const boxesRef = document.querySelector('#boxes');
+console.log(boxesRef);
 
-let amount = 0;
+let defaultSize = 30;
 
-inputNumber.addEventListener('input', () => {
+renderBtn.addEventListener('click', handleRenderBtnClick);
 
- amount = inputNumber.value;
-
- console.log(amount);
-
-});
+destroyBtn.addEventListener('click', handleDestroyBtnClick);
 
 
 
+function handleRenderBtnClick () {
 
-renderBtn.addEventListener('click', () => {
-    createBoxes(amount)
-};
-
-function createBoxes(amount) {
-    for (let i = 0; i < amount; i+1){
-
-    }
+    const amount = Number(inputNumber.value);
+    createBoxes(amount);
 }
 
-// destroyBtn.addEventListener('click', ());
+function createBoxes(amount) {
+    const collection = []
+    for (let i = 0; i < amount; i +1){
+        
+     const div = document.createElement('div');
+     div.style.backgroundColor = createRandom();
+     div.style.width = defaultSize + 'px';
+     div.style.height = defaultSize + 'px';
+     defaultSize += 10;
+     console.log(div);
+     collection.push(div);
+    }
+
+    boxesRef.append(...collection);
+}
+
+function createRandom() {
+    return `rgb(${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0})`;
+}
+
+function handleDestroyBtnClick() {
+
+}
+
